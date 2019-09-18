ff10b071523df2f9336fb1d4b26f998f60dcd643
nathanielbaldevino32 <52551740+nathanielbaldevino32@users.noreply.github.com>
2019-09-19 04:48:25 +0800

    Add files via upload

---
+++ cpe40032-m5-18/src/Step.lua
@@ -0,0 +1,26 @@
+--[[
+    CMPE40032
+    -- Super Mario Bros. Remake --
+
+]]
+
+
+Step = Class{}
+
+function Step:init(x, y, dx, dy, direction, state, score, hasKey, removed)
+    self.x = x
+    self.y = y
+
+    self.dx = dx
+    self.dy = dy
+
+    self.direction = direction
+    self.state = state
+
+    self.isState = false
+
+    self.score = score
+    self.hasKey = hasKey
+
+    self.removed = removed
+end
---
+++ cpe40032-m5-18/src/states/entity/PlayerPausedState.lua
@@ -0,0 +1,21 @@
+--[[
+    CMPE40032
+    Super Mario Bros. Remake
+
+]]
+
+PlayerPausedState = Class{__includes = BaseState}
+
+function PlayerPausedState:init(player)
+    self.player = player
+
+    self.animation = Animation {
+        frames = {1},
+        interval = 1
+    }
+
+    self.player.currentAnimation = self.animation
+end
+
+function PlayerPausedState:update(dt)
+end
